98fe21ebb185719c255ac9baa51b536b02ed5809
David <contato.davidalmeida@outlook.com>
2024-03-11 17:33:45 -0300

    🎉 Begin a project

---
+++ src/http/server.ts
@@ -0,0 +1,26 @@
+import fastify from "fastify";
+import z from "zod";
+import { PrismaClient } from "@prisma/client";
+
+const app = fastify();
+const prisma = new PrismaClient();
+
+app.post("/polls", async (req, res) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+
+  const { title } = createPollBody.parse(req.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return res.status(201).send({ pollId: poll.id  });
+});
+
+app.listen({ port: 3333 }).then((port) => {
+  console.log("Server running on port", port);
+});
